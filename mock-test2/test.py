9c5372a7069d18d0789f56609b8e64b2849563a9
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-03-16 12:19:06 +0100

    2nd demo that use unittest.mock library

---
+++ mock-test2/test.py
@@ -0,0 +1,54 @@
+from unittest.mock import *
+
+import application
+
+
+def test1():
+    print(application.function1())
+
+
+@patch('application.function1', return_value=42)
+def test2(mocked_function):
+    print(application.function1())
+
+
+def side_effect_handler():
+    print("side_effect function called")
+    return -1
+
+
+@patch('application.function1', side_effect=side_effect_handler)
+def test3(mocked_function):
+    print(application.function1())
+
+
+@patch('application.function1', return_value=42, side_effect=side_effect_handler)
+def test4(mocked_function):
+    print(application.function1())
+
+
+def side_effect_handler_2():
+    print("side_effect function called")
+    return DEFAULT
+
+
+@patch('application.function1', return_value=42, side_effect=side_effect_handler_2)
+def test5(mocked_function):
+    print(application.function1())
+
+
+if __name__ == '__main__':
+    test1()
+    print()
+
+    test2()
+    print()
+
+    test3()
+    print()
+
+    test4()
+    print()
+
+    test5()
+    print()
