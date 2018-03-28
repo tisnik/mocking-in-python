61e03d5f85fafe1fd026ad41c9c5d3f932b5da05
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-03-28 10:27:41 +0200

    Main module for all old examples

---
+++ mock-test1/main.py
@@ -0,0 +1,5 @@
+from application import *
+
+
+if __name__ == '__main__':
+    print(function1())
---
+++ mock-test6/main.py
@@ -0,0 +1,7 @@
+from application import Application
+
+
+if __name__ == '__main__':
+    app = Application()
+    app.method1()
+    app.method2()
