d3c8905aaa12211d1c5f313f826e67f107fd3600
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-05-04 12:26:07 +0200

    Added comments to all Python scripts

--- mock-test1/test.py
+++ mock-test1/test.py
@@ -1,14 +1,18 @@
+"""Implementace (umělých) jednotkových testů."""
+
 from unittest.mock import *
 
 import application
 
 
 def test1():
+    """První test neprovádí prakticky žádné reálné kontroly, jen zavolá testovanou funkci."""
     print(application.function1())
 
 
 @patch('application.function1', return_value=42)
 def test2(mocked_function):
+    """Druhý test používá fake test double - náhradu volané funkce."""
     print(application.function1())
 
 
